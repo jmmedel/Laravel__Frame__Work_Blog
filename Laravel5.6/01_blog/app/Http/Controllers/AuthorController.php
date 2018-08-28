bcd484a580c4953deb5e4e136a170758dc6a78dd
john <jm.kagaya@gmail.com>
2018-08-28 12:54:02 +0900

    Done
    Done

--- Laravel5.6/01_blog/app/Http/Controllers/AuthorController.php
+++ Laravel5.6/01_blog/app/Http/Controllers/AuthorController.php
@@ -4,6 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Post;
+// you need to add this manual 
+use App\Comment;
+
+use Carbon\Carbon;
+// you need to add this manual in visual studio code this is not automatic
+use Illuminate\Support\Facades\Auth;
+
 class AuthorController extends Controller
 {
     //
@@ -17,7 +25,10 @@ class AuthorController extends Controller
 
     public function dashboard(){
 
-        return view('author.dashboard');
+        $posts = Post::where('user_id',Auth::id())->pluck('id')->toArray();
+        $allcomments = Comment::whereIn('post_id',$posts)->get();
+        $todayComments = $allcomments->where('created_at','>=',Carbon::today())->count();
+        return view('author.dashboard',compact('allcomments','todayComments'));
 
     }
 
--- Laravel5.6/01_blog/resources/views/author/dashboard.blade.php
+++ Laravel5.6/01_blog/resources/views/author/dashboard.blade.php
@@ -26,12 +26,12 @@
                     <div class="card p-4">
                         <div class="card-body d-flex justify-content-between align-items-center">
                             <div>
-                                <span class="h4 d-block font-weight-normal mb-2">$32,400</span>
-                                <span class="font-weight-light">Income</span>
+                                <span class="h4 d-block font-weight-normal mb-2">{{ Auth::user()->posts->count() }}</span>
+                                <span class="font-weight-light">POST All Time</span>
                             </div>
 
                             <div class="h2 text-muted">
-                                <i class="icon icon-wallet"></i>
+                                <i class="icon icon-paper-plane"></i>
                             </div>
                         </div>
                     </div>
@@ -41,8 +41,8 @@
                     <div class="card p-4">
                         <div class="card-body d-flex justify-content-between align-items-center">
                             <div>
-                                <span class="h4 d-block font-weight-normal mb-2">900</span>
-                                <span class="font-weight-light">Downloads</span>
+                                <span class="h4 d-block font-weight-normal mb-2">{{  $todayComments  }}</span>
+                                <span class="font-weight-light">Comments today </span>
                             </div>
 
                             <div class="h2 text-muted">
@@ -56,8 +56,8 @@
                     <div class="card p-4">
                         <div class="card-body d-flex justify-content-between align-items-center">
                             <div>
-                                <span class="h4 d-block font-weight-normal mb-2">32s</span>
-                                <span class="font-weight-light">Time</span>
+                                <span class="h4 d-block font-weight-normal mb-2">{{ $allcomments->count() }}</span>
+                                <span class="font-weight-light">Comments all time </span>
                             </div>
 
                             <div class="h2 text-muted">
@@ -72,35 +72,11 @@
                 <div class="col-md-12">
                     <div class="card">
                         <div class="card-header">
-                            Total Users
+                            Post by day
                         </div>
 
                         <div class="card-body p-0">
-                            <div class="p-4">
-                                <canvas id="line-chart" width="100%" height="20"></canvas>
-                            </div>
-
-                            <div class="justify-content-around mt-4 p-4 bg-light d-flex border-top d-md-down-none">
-                                <div class="text-center">
-                                    <div class="text-muted small">Total Traffic</div>
-                                    <div>12,457 Users (40%)</div>
-                                </div>
-
-                                <div class="text-center">
-                                    <div class="text-muted small">Banned Users</div>
-                                    <div>95,333 Users (5%)</div>
-                                </div>
-
-                                <div class="text-center">
-                                    <div class="text-muted small">Page Views</div>
-                                    <div>957,565 Pages (50%)</div>
-                                </div>
-
-                                <div class="text-center">
-                                    <div class="text-muted small">Total Downloads</div>
-                                    <div>957,565 Files (100 TB)</div>
-                                </div>
-                            </div>
+                            
                         </div>
                     </div>
                 </div>
