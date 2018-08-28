b5a585873aed453c105dabc4c4845fd5607fead5
john <jm.kagaya@gmail.com>
2018-08-28 12:26:05 +0900

    done
    done

--- Laravel5.6/01_blog/app/User.php
+++ Laravel5.6/01_blog/app/User.php
@@ -5,7 +5,7 @@ namespace App;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
-
+use Carbon\Carbon;
 class User extends Authenticatable
 {
     use Notifiable;
