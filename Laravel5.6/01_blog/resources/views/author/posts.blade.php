9e5dbf16406eb7342aff9686ddc2c58adb701449
john <jm.kagaya@gmail.com>
2018-09-04 07:53:28 +0900

    Done
    Done

--- Laravel5.6/01_blog/resources/views/author/posts.blade.php
+++ Laravel5.6/01_blog/resources/views/author/posts.blade.php
@@ -4,6 +4,42 @@
 
 @section('content')
 
+<div class="card">
+    <div class="card-header bg-light">
+        Author Posts
+    </div>
 
+    <div class="card-body">
+        <div class="table-responsive">
+            <table class="table table-striped">
+                <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Title</th>
+                    <th>Content</th>
+                    <th>Created</th>
+                    <th>Action</th>
+                </tr>
+                </thead>
+                <tbody>
+                @foreach(Auth::user()->comments as $comment)
+                <tr>
+                    <td>{{ $comment->id }}</td>
+                    <td class="text-nowrap"><a href="{{ route('singlePost',$comment->id) }}">{{ $comment->post->title }}</a></td>
+                    <td>{{ $comment->content }}</td>
+                    <td>{{ \Carbon\Carbon::parse($comment->created_at)->diffForHumans() }}</td>
+                    <td>
+                        <form id="deleteComment-{{ $comment->id }}" action="{{ route('deleteComment',$comment->id) }}" method="POST">@csrf</form>
+                        <button type="button" class="btn btn-danger" onclick="document.getElementById('deleteComment-{{ $comment->id }}').submit()">X</button>
+                    </td>
+                </tr>
+               
+                @endforeach
+               
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
 
 @endsection
