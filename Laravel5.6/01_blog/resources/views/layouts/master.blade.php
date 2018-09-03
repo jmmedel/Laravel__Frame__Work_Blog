1929dfdb7f4d32542c27da4fd187672adc556fb3
john <jm.kagaya@gmail.com>
2018-09-04 07:45:51 +0900

    Done
    done

--- Laravel5.6/01_blog/resources/views/layouts/master.blade.php
+++ Laravel5.6/01_blog/resources/views/layouts/master.blade.php
@@ -38,7 +38,7 @@
     <script src="{{ asset('assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
 
     <!-- Custom scripts for this template -->
-    <script src="{{ asset('js/clean-blog.min.js') }}"></script>
+    <script src="{{ asset('assets/js/clean-blog.min.js') }}"></script>
 
     
 
