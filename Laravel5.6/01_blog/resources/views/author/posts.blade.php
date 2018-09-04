8bdd1ccc13a084f3ca62e074187e1ff8a73f4da4
john <jm.kagaya@gmail.com>
2018-09-05 08:01:48 +0900

    done
    Done

--- Laravel5.6/01_blog/resources/views/author/posts.blade.php
+++ Laravel5.6/01_blog/resources/views/author/posts.blade.php
@@ -34,8 +34,8 @@
                     <td>{{ $post->comments->count() }}</td>
                     <td>
                         <a href="{{ route('postEdit',$post->id) }}" class="btn-warning"> Edit</a>
-                        <form  id="deletePost-{{ $post->id }}" action="{{ route('deletePost',$post->id) }}" method="POST" >@csrf</form>
-                        <a href="#" onclick="document.getElementById('deletePost-{{ $post->id }}').submit()" class="btn-danger"> Remove</a>
+                        
+                        <a href="#"  class="btn-danger" data-toggle="modal" data-target="#deletePostModal-{{ $post->id }}" > Remove</a>
                     </td>
                 </tr>
                
@@ -50,6 +50,30 @@
 </div>
 
 
+@foreach (Auth::user()->posts as $post)
+<!-- Modal -->
 
+<div class="modal fade" id="deletePostModal-{{ $post->id }}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">You are about to delete {{ $post->title }}</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        Are You sure?
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">No Keep it </button>
+        <form  id="deletePost-{{ $post->id }}" action="{{ route('deletePost',$post->id) }}" method="POST" >@csrf
+        <button type="submit" class="btn btn-primary">Yes, Delete it </button>
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
+@endforeach
 
 @endsection
