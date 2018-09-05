42f206e29b6a37728c16e6a0ccae0fd1564262e8
john <jm.kagaya@gmail.com>
2018-09-05 22:20:51 +0900

    Done
    Done

--- Laravel5.6/02_Web_Shop/resources/views/shop/index.blade.php
+++ Laravel5.6/02_Web_Shop/resources/views/shop/index.blade.php
@@ -1,20 +1,24 @@
 
 @extends('layouts.master')
 
+@section('content')
+    
+
 <!-- Page Header -->
-<header class="masthead" style="background-image: url('{{ asset('assets/img/home-bg.jpg') }}' ")>
+<header class="masthead" style="background-image: url('{{ asset('img/assets/GU.png') }}' ")>
   <div class="overlay"></div>
   <div class="container">
     <div class="row">
       <div class="col-lg-8 col-md-10 mx-auto">
         <div class="site-heading">
-          <h1>Kagaya Blog</h1>
-          <span class="subheading"> {{ remove_spaces('Personal blog') }} </span>
+          <h1>Shop</h1>
+          <span class="subheading"> 'Super Cool T shirt ' </span>
         </div>
       </div>
     </div>
   </div>
 </header>
+
 
 @endsection
 
