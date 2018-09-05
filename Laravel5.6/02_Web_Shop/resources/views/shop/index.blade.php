7594d980405987eba5960b8e265b7ffe7c9f2508
john <jm.kagaya@gmail.com>
2018-09-05 22:12:40 +0900

    Done
    Done

---
+++ Laravel5.6/02_Web_Shop/resources/views/shop/index.blade.php
@@ -0,0 +1,19 @@
+
+@extends('layouts.master')
+
+<!-- Page Header -->
+<header class="masthead" style="background-image: url('{{ asset('assets/img/home-bg.jpg') }}' ")>
+  <div class="overlay"></div>
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-8 col-md-10 mx-auto">
+        <div class="site-heading">
+          <h1>Kagaya Blog</h1>
+          <span class="subheading"> {{ remove_spaces('Personal blog') }} </span>
+        </div>
+      </div>
+    </div>
+  </div>
+</header>
+
+
