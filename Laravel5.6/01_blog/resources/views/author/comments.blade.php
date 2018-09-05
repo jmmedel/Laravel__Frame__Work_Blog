e87194b9fd2ff372e8e307cd7876b0a26c1ae764
kjmkagaya <jm.kagaya@gmail.com>
2018-09-05 09:29:54 +0900

    Done
    Done

--- Laravel5.6/01_blog/resources/views/author/comments.blade.php
+++ Laravel5.6/01_blog/resources/views/author/comments.blade.php
@@ -1,2 +1,53 @@
 @extends('layouts.admin')
-author comments
+
+
+@section('title') Author Comments @endsection
+
+
+@section('content')
+
+<div class="content">
+        <div class="card">
+                <div class="card-header bg-light">
+                    Author Comments
+                </div>
+        
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table class="table table-striped">
+                            <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Post</th>
+                                <th>Content</th>
+                                <th>Created</th>
+                                <th>Action</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            @foreach(Auth::user()->comments as $comment)
+                            <tr>
+                                <td>{{ $comment->id }}</td>
+                                <td class="text-nowrap"><a href="{{ route('singlePost',$comment->id) }}">{{ $comment->post->title }}</a></td>
+                                <td>{{ $comment->content }}</td>
+                                <td>{{ \Carbon\Carbon::parse($comment->created_at)->diffForHumans() }}</td>
+                                <td>
+                                    
+                                    <button type="button" class="btn btn-danger"  data-toggle="modal" data-target="#deleteCommentModal-{{ $comment->id }}">X</button>
+                                </td>
+                            </tr>
+                           
+                            @endforeach
+                           
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+        </div>
+</div>
+
+
+
+@endsection
+
+
