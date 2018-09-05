9a96aa8cc59ccb9890c94b80de3bfd5d2838213c
john <jm.kagaya@gmail.com>
2018-09-05 22:30:25 +0900

    done
    Done

--- Laravel5.6/02_Web_Shop/database/migrations/2018_09_05_132553_create_products_table.php
+++ Laravel5.6/02_Web_Shop/database/migrations/2018_09_05_132553_create_products_table.php
@@ -15,6 +15,10 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->increments('id');
+            $table->string();
+            $table->string();
+            $table->text();
+            $table->double();
             $table->timestamps();
         });
     }
