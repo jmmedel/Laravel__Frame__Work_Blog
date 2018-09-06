5ef4f07c32e56789261b52a96416cae0d51badf7
john <jm.kagaya@gmail.com>
2018-09-07 04:27:10 +0900

    Done
    Done

--- Laravel5.6/02_Web_Shop/resources/views/admin/products.blade.php
+++ Laravel5.6/02_Web_Shop/resources/views/admin/products.blade.php
@@ -50,6 +50,30 @@
 
 </div>
 
+@foreach ($posts as $post)
+<!-- Modal -->
 
+<div class="modal fade" id="deletePostModal-{{ $post->id }}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">You are about to delete {{ $post->title }}</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        Are You sure?
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">No Keep it </button>
+        <form  id="deletePost-{{ $post->id }}" action="{{ route('admindeletePost',$post->id) }}" method="POST" >@csrf
+        <button type="submit" class="btn btn-primary">Yes, Delete it </button>
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
+@endforeach
 
 @endsection
