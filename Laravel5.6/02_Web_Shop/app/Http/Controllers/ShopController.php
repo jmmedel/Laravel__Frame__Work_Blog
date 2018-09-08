e8d6164e4e28c47bc8215935d21a4c50c644070c
john <jm.kagaya@gmail.com>
2018-09-09 05:16:41 +0900

    Done
    Done

--- Laravel5.6/02_Web_Shop/app/Http/Controllers/ShopController.php
+++ Laravel5.6/02_Web_Shop/app/Http/Controllers/ShopController.php
@@ -17,6 +17,13 @@ use PayPal\Api\RedirectUrls;
 use PayPal\Api\Transaction;
 
 use App\Facade\PayPal;
+
+
+
+
+use PayPal\Api\PaymentExecution;
+
+
 class ShopController extends Controller
 {
     //
@@ -98,8 +105,8 @@ $transaction->setAmount($amount)
 // payment approval/ cancellation.
 $baseUrl = "http://127.0.0.1:8000";
 $redirectUrls = new RedirectUrls();
-$redirectUrls->setReturnUrl("$baseUrl/ExecutePayment.php?success=true")
-    ->setCancelUrl("$baseUrl/ExecutePayment.php?success=false");
+$redirectUrls->setReturnUrl()
+    ->setCancelUrl();
 
 // ### Payment
 // A Payment Resource; create one using
@@ -140,5 +147,82 @@ $approvalUrl = $payment->getApprovalLink();
 
 return $payment;
     }
+
+
+    public function executeOrder(){
+
+        $apiContext = Paypal::apiContext();
+
+
+        if (isset($_GET['success']) && $_GET['success'] == 'true') {
+
+            // Get the payment Object by passing paymentId
+            // payment id was previously stored in session in
+            // CreatePaymentUsingPayPal.php
+            $paymentId = $_GET['paymentId'];
+            $payment = Payment::get($paymentId, $apiContext);
+        
+            // ### Payment Execute
+            // PaymentExecution object includes information necessary
+            // to execute a PayPal account payment.
+            // The payer_id is added to the request query parameters
+            // when the user is redirected from paypal back to your site
+            $execution = new PaymentExecution();
+            $execution->setPayerId($_GET['PayerID']);
+        
+            // ### Optional Changes to Amount
+            // If you wish to update the amount that you wish to charge the customer,
+            // based on the shipping address or any other reason, you could
+            // do that by passing the transaction object with just `amount` field in it.
+            // Here is the example on how we changed the shipping to $1 more than before.
+            $transaction = new Transaction();
+            $amount = new Amount();
+            $details = new Details();
+        
+            $details->setShipping(2.2)
+                ->setTax(1.3)
+                ->setSubtotal(17.50);
+        
+            $amount->setCurrency('USD');
+            $amount->setTotal(21);
+            $amount->setDetails($details);
+            $transaction->setAmount($amount);
+        
+            // Add the above transaction object inside our Execution object.
+            $execution->addTransaction($transaction);
+        
+            try {
+                // Execute the payment
+                // (See bootstrap.php for more on `ApiContext`)
+                $result = $payment->execute($execution, $apiContext);
+        
+                // NOTE: PLEASE DO NOT USE RESULTPRINTER CLASS IN YOUR ORIGINAL CODE. FOR SAMPLE ONLY
+               printf("Executed Payment 1". $payment->getId()."Results: ".$result);
+        
+                try {
+                    $payment = Payment::get($paymentId, $apiContext);
+                } catch (\Exception $ex) {
+                    // NOTE: PLEASE DO NOT USE RESULTPRINTER CLASS IN YOUR ORIGINAL CODE. FOR SAMPLE ONLY
+                    printf("Get Payment 1");
+                    exit(1);
+                }
+            } catch (\Exception $ex) {
+                // NOTE: PLEASE DO NOT USE RESULTPRINTER CLASS IN YOUR ORIGINAL CODE. FOR SAMPLE ONLY
+               printf("Executed Payment");
+                exit(1);
+            }
+        
+            // NOTE: PLEASE DO NOT USE RESULTPRINTER CLASS IN YOUR ORIGINAL CODE. FOR SAMPLE ONLY
+           printf("Get Payment 2 " . $payment->getId());
+        
+            return $payment;
+        } else {
+            // NOTE: PLEASE DO NOT USE RESULTPRINTER CLASS IN YOUR ORIGINAL CODE. FOR SAMPLE ONLY
+            printf("User Cancelled the Approval", null);
+            exit;
+        }
+
+    }
+
 
 }
