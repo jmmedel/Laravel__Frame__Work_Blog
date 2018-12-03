c7aec396a0653debc1d024287ae091efe1b759d5
kjmkagaya <jm.kagaya@gmail.com>
2018-12-03 14:47:01 +0900

    Fix the navigation
    Fix the navigation bar

--- Laravel5.6/02_Web_Shop/resources/views/user/public_profile.blade.php
+++ Laravel5.6/02_Web_Shop/resources/views/user/public_profile.blade.php
@@ -12,6 +12,35 @@
 <body class="bg-grey-light font-sans">
   
   
+<nav class="flex items-center justify-between flex-wrap bg-teal p-6">
+    <div class="flex items-center flex-no-shrink text-white mr-6">
+      
+      <svg class="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/></svg>
+      <span class="font-semibold text-xl tracking-tight">Tailwind CSS</span>
+    </div>
+    <div class="block lg:hidden">
+      <button class="flex items-center px-3 py-2 border rounded text-teal-lighter border-teal-light hover:text-white hover:border-white">
+        <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+      </button>
+    </div>
+    <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+      <div class="text-sm lg:flex-grow">
+        <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-lighter hover:text-white mr-4">
+          Docs
+        </a>
+        <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-lighter hover:text-white mr-4">
+          Examples
+        </a>
+        <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-lighter hover:text-white">
+          Blog
+        </a>
+        
+    
+      </div>
+      
+    </div>
+  </nav>
+
   
 
   <div class="hero h-64 bg-cover h-112"></div>
@@ -49,7 +78,7 @@
                   </li>
                   <li class="text-center py-3 px-4 border-b-2 border-solid border-transparent hover:border-teal">
                       <a href="#" class="text-grey-darker no-underline hover:no-underline">
-                          <div class="text-sm font-bold tracking-tight mb-1">Moments</div>
+                          <div class="text-sm font-bold tracking-tight mb-1">Dislike</div>
                           <div class="text-lg tracking-tight font-bold hover:text-teal">1</div>
                       </a>
                   </li>
@@ -57,7 +86,8 @@
           </div>
           <div class="w-full lg:w-1/4 flex my-4 lg:my-0 lg:justify-end items-center">
               <div class="mr-6">
-                  <button class="bg-teal hover:bg-teal-dark text-white font-medium py-2 px-4 rounded-full">Following</button>
+                  <button class="bg-teal hover:bg-teal-dark text-white font-medium py-2 px-4 rounded-full">Make Post</button>
+                  <button class="bg-teal hover:bg-teal-dark text-white font-medium py-2 px-4 rounded-full">Go HOME</button>
               </div>
               <div>
                   <a href="#" class="text-grey-dark"><i class="fa fa-ellipsis-v fa-lg"></i></a>
@@ -113,7 +143,7 @@
       <div class="w-full lg:w-1/2 bg-white mb-4">
 
           <div class="p-3 text-lg font-bold border-b border-solid border-grey-light">
-              <a href="#" class="text-black mr-6 no-underline hover-underline">Tweets</a>
+              <a href="#" class="text-black mr-6 no-underline hover-underline">POST</a>
               <a href="#" class="mr-6 text-teal no-underline hover:underline">Tweets &amp; Replies</a>
               <a href="#" class="text-teal no-underline hover:underline">Media</a>
           </div>
