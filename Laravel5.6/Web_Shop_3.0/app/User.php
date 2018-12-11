1f70b3303e0903cf533820ba24dedcd0aa5b9aba
jm.kagaya@gmail.com <jm.kagaya@gmail.com>
2018-12-11 09:28:10 +0900

    add some question
    add some desgin on this page
    using url to get the next page

--- Laravel5.6/Web_Shop_3.0/app/User.php
+++ Laravel5.6/Web_Shop_3.0/app/User.php
@@ -46,6 +46,11 @@ class User extends Authenticatable
         return $this->hasMany('App\Question');
     }
 
-   
+    public function getUrlAttribute()
+    {
+
+       //return route("questions.show", $this->id);
+       return '#';
+    }
 
 }
