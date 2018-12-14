2ae508b91289f958b37dae1a6e57e707fe592356
jm.kagaya@gmail.com <jm.kagaya@gmail.com>
2018-12-14 09:35:16 +0900

    Done
    Done

--- Laravel5.6/Web_Shop_3.0/resources/views/welcome.blade.php
+++ Laravel5.6/Web_Shop_3.0/resources/views/welcome.blade.php
@@ -53,7 +53,7 @@
 
         <!-- Sidebar Widgets Column -->
         <!-- col-md-4 -->
-        <div class="col-md-4">
+        <div class="col-md-4 ">
 
           <!-- Search Widget -->
           <div class="card my-4">
@@ -73,7 +73,7 @@
             <h5 class="card-header">Categories</h5>
             <div class="card-body">
               <div class="row">
-                <div class="col-lg-6">
+                <div class="col-lg-6 ">
                   <ul class="list-unstyled mb-0">
                     <li>
                       <a href="#">Web Design</a>
