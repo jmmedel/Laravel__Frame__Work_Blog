362253b5036f9743463c2a256d6020e197c2db3d
jm.kagaya@gmail.com <jm.kagaya@gmail.com>
2018-12-14 19:13:51 +0900

    add some asnwer question
    add some answer question in the views

--- Laravel5.6/Web_Shop_3.0/app/Answer.php
+++ Laravel5.6/Web_Shop_3.0/app/Answer.php
@@ -36,15 +36,18 @@ class Answer extends Model
         static::created(function($answer){
 
            $answer->question->increment('answers_count');
-           
+
 
         });
 
 
-       
-
     }
 
+    public function getCreatedDateAttribute()
+    {
+        //
+        return $this->created_at->diffForHumans();
 
+    }
 
 }
