7e1d40084fa061f17e8ca088f3ac809ea3b1819c
jm.kagaya@gmail.com <jm.kagaya@gmail.com>
2018-12-27 00:44:25 +0900

    done 3.0
    done release 3.0

--- Laravel5.6/Web_Shop_3.0/resources/views/includes/footer.blade.php
+++ Laravel5.6/Web_Shop_3.0/resources/views/includes/footer.blade.php
@@ -28,6 +28,22 @@
                   </span>
                 </a>
               </li>
+              <li class="list-inline-item">
+                <a href="#">
+                  <span class="fa-stack fa-lg">
+                    <i class="fa fa-circle fa-stack-2x"></i>
+                    <i class="fa fa-youtube fa-stack-1x fa-inverse"></i>
+                  </span>
+                </a>
+              </li>
+              <li class="list-inline-item">
+                <a href="#">
+                  <span class="fa-stack fa-lg">
+                    <i class="fa fa-circle fa-stack-2x"></i>
+                    <i class="fa fa-instagram fa-stack-1x fa-inverse"></i>
+                  </span>
+                </a>
+              </li>
             </ul>
             <p class="copyright text-muted">Copyright &copy; Kagaya john 2018</p>
           </div>
