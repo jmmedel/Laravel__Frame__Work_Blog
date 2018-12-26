f4626571f5ae8f459094751884bac26833642337
jm.kagaya@gmail.com <jm.kagaya@gmail.com>
2018-12-26 21:10:09 +0900

    fix the porifle image update
    done fix the profile image update

--- Laravel5.6/Web_Shop_3.0/app/Http/Controllers/UserControllerProfile.php
+++ Laravel5.6/Web_Shop_3.0/app/Http/Controllers/UserControllerProfile.php
@@ -14,12 +14,12 @@ class UserControllerProfile extends Controller
     }
     public function update_avatar(Request $request){
     	// Handle the user upload of avatar
-    	if($request->hasFile('avatar')){
-    		$avatar = $request->file('avatar');
-    		$filename = time() . '.' . $avatar->getClientOriginalExtension();
-    		Image::make($avatar)->resize(300, 300)->save( public_path('/uploads/avatars/' . $filename ) );
+    	if($request->hasFile('avatars')){
+    		$avatars = $request->file('avatars');
+    		$filename = time() . '.' . $avatars->getClientOriginalExtension();
+    		Image::make($avatars)->resize(300, 300)->save( public_path('/uploads/avatars/' . $filename ) );
     		$user = Auth::user();
-    		$user->avatar = $filename;
+    		$user->avatars = $filename;
     		$user->save();
     	}
     	return view('profile', array('user' => Auth::user()) );
--- Laravel5.6/Web_Shop_3.0/resources/views/profile.blade.php
+++ Laravel5.6/Web_Shop_3.0/resources/views/profile.blade.php
@@ -4,11 +4,11 @@
 <div class="container">
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
-            <img src="/uploads/avatars/{{ $user->avatar }}" style="width:150px; height:150px; float:left; border-radius:50%; margin-right:25px;">
+            <img src="/uploads/avatars/{{ $user->avatars }}" style="width:150px; height:150px; float:left; border-radius:50%; margin-right:25px;">
             <h2>{{ $user->name }}s Profile</h2>
             <form enctype="multipart/form-data" action="/updateprofile" method="POST">
                 <label>Update Profile Image</label>
-                <input type="file" name="avatar">
+                <input type="file" name="avatars">
                 <input type="hidden" name="_token" value="{{ csrf_token() }}">
                 <input type="submit" class="pull-right btn btn-sm btn-primary">
             </form>
